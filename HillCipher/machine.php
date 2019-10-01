e948541c134cd7aab0cd35bec902442adccf4d45
DjatikusumaID <djatikusumadata@gmail.com>
2019-10-01 21:09:35 +0700

    Create new class

---
+++ HillCipher/machine.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Aplikasi Matriks - Hill Cipher
+ * Tugas Besar Matematika Diskrit
+ * Dosen :
+ * 		Nelly Indriani W, S.Kom, M.Kom
+ * Dibuat oleh :
+ * 		M. Iqbal Musyaffa
+ *   	Rangga DJjatikusuma Lukman
+ *    	Farhan Mubarok
+ */
+class IRF_machine{
+	
+}
